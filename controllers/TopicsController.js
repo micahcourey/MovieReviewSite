0e50e888fee5fcd3c9b1599ad003d6467a045ec1
micahcourey <micahjcourey@gmail.com>
2015-10-02 13:49:54 -0700

    hard code movie in factory and display review

--- controllers/TopicsController.js
+++ controllers/TopicsController.js
@@ -1,4 +1,4 @@
 movieSite.controller('TopicsCtrl', function TopicsCtrl($scope, MoviesFactory) {
   $scope.topics = MoviesFactory.topics;
   $scope.MoviesFactory = MoviesFactory;
-})
+});
--- services/MoviesFactory.js
+++ services/MoviesFactory.js
@@ -1,10 +1,16 @@
 movieSite.factory('MoviesFactory', function MoviesFactory() {
   var factory = {};
-  factory.topics = [];
+  factory.topics = [
+    {
+      title: "Don't ever see Garden State",
+      body: "blah blah blah", 
+    }
+  ];
 
   factory.addTopic = function() {
     factory.topics.push({ title: factory.topicTitle, body: factory.topicBody });
     factory.topicTitle = null;
+    factory.topicBody = null;
   };
   return factory;
 });
