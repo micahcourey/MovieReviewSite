512f6407beabf87b6aa5aa8a3836e2bee392872c
micahcourey <micahjcourey@gmail.com>
2015-10-02 16:40:10 -0700

    add css remove uneeded files

--- controllers/DiscussionsController.js
+++
@@ -1,7 +0,0 @@
-movieSite.controller('DiscussionsCtrl', function DiscussionsCtrl($scope, MoviesFactory) {
-  // $scope.topic = UtilitesFactory.findById(MoviesFactory.topic, $stateParams.courseId)
-//   $scope.addDiscourse = function() {
-//     $scope.topic.discussions.push{{ discourse: $scope.discourse }};
-//     $scope.discourse = null;
-//   }
-});
---
+++ directives/MyDirectives.js
@@ -0,0 +1,7 @@
+movieSite.directive("changeClass", function() {
+  return function(scope, element) {
+    element.bind("click", function() {
+      element.toggleClass("darkmode");
+    });
+  }
+});
